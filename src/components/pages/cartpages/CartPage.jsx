7ef4cc210954de5116594ef73a8e90bac0f1bf74
Lee gangho <rkdgh1190@gmail.com>
2024-06-05 20:02:56 +0900

    Add to image

--- src/components/pages/cartpages/CartPage.jsx
+++ src/components/pages/cartpages/CartPage.jsx
@@ -4,9 +4,9 @@ import NavBar from '../../organisms/nav/NavBar';
 
 const CartPage = () => {
   const items = [
-    { id: 1, product: { name: '너무나 좋은 해변 수건', description: '흡수성이 뛰어난 면 수건', price: 19.99, image: 'path/to/image1.jpg' } },
-    { id: 2, product: { name: '향긋한 애플 사이다 시럽', description: '맛있는 시럽으로 음료를 업그레이드하세요', price: 12.99, image: 'path/to/image2.jpg' } },
-    { id: 3, product: { name: '가을 패션 모자', description: '스타일리시한 모자로 가을 옷차림을 완성하세요', price: 24.99, image: 'path/to/image3.jpg' } }
+    { id: 1, product: { name: '너무나 좋은 해변 수건', description: '흡수성이 뛰어난 면 수건', price: 19.99, image: 'https://png.pngtree.com/png-clipart/20211116/original/pngtree-mens-shirts-clothes-cotton-fabrics-warm-clothes-coats-and-textiles-png-image_6933125.png' } },
+    { id: 2, product: { name: '향긋한 애플 사이다 시럽', description: '맛있는 시럽으로 음료를 업그레이드하세요', price: 12.99, image: 'https://png.pngtree.com/png-clipart/20211116/original/pngtree-mens-shirts-clothes-cotton-fabrics-warm-clothes-coats-and-textiles-png-image_6933125.png' } },
+    { id: 3, product: { name: '가을 패션 모자', description: '스타일리시한 모자로 가을 옷차림을 완성하세요', price: 24.99, image: 'https://png.pngtree.com/png-clipart/20211116/original/pngtree-mens-shirts-clothes-cotton-fabrics-warm-clothes-coats-and-textiles-png-image_6933125.png' } }
   ];
 
   const user = {
