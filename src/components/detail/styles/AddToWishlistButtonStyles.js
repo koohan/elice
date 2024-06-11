3c16926912d3318c47dfd0059691cbd9c623d475
[IoT3] 이다은 <lgc6466@naver.com>
2024-06-12 00:50:12 +0900

    detail page 디자인 수정

--- src/components/detail/styles/AddToWishlistButtonStyles.js
+++ src/components/detail/styles/AddToWishlistButtonStyles.js
@@ -1,24 +1,64 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const shinyBtn1 = keyframes`
+  0% { transform: scale(0) rotate(45deg); opacity: 0; }
+  80% { transform: scale(0) rotate(45deg); opacity: 0.5; }
+  81% { transform: scale(4) rotate(45deg); opacity: 1; }
+  100% { transform: scale(50) rotate(45deg); opacity: 0; }
+`;
 
 export const CheckoutButton = styled.button`
+  position: relative;
   padding: 15px;
-  background: #2ecc71;
-  border-radius: 5px;
-  color: #fff;
+  background: #FFE07A;
   border: none;
+  border-radius: 5px;
+  color: #C27319;
+  overflow: hidden;
   cursor: pointer;
   width: 50%;
   font-size: 16px;
   margin-bottom: 10px;
+  font-family: var(--font-nanumfont);
+
   &:hover {
-    background: #27ae60;
+    text-decoration: none;
+    color: #fff;
+    background-color: #FFC45A;
+  }
+
+  &:active {
+    box-shadow: 4px 4px 6px 0 rgba(255,255,255,0.3),
+                -4px -4px 6px 0 rgba(116, 125, 136, 0.2), 
+                inset -4px -4px 6px 0 rgba(255,255,255,0.2),
+                inset 4px 4px 6px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  &:before {
+    position: absolute;
+    content: '';
+    display: inline-block;
+    top: -180px;
+    left: 0;
+    width: 30px;
+    height: 100%;
+    background-color: #fff;
+    animation: ${shinyBtn1} 3s ease-in-out infinite;
   }
 `;
 
 export const WishlistButton = styled(CheckoutButton)`
-  background: #3498db;
+  background: #7485BC;
   border-radius: 5px;
+  color: #fff;
+  font-family: var(--font-nanumfont);
+
   &:hover {
-    background: #2980b9;
+    background: #D1DCFD;
+    color: #506BC1;
+  }
+
+  &:before {
+    display: none;
   }
 `;
