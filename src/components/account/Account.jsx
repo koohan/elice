e96d6a83f603ebb7fa54febdcf691c10f4d8d3c1
Lee gangho <rkdgh1190@gmail.com>
2024-06-13 11:13:37 +0900

    Modify Navbar State

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import PersonalInfo from './PersonalInfo';
 import PasswordChange from './PasswordChange';
-import {
-  
-    Button,
-    DelBtn,
+import { Button, DelBtn } from "./styles/PasswordChangeStyles";
+import useAccount from '../../hook/useAccount';
 
-  } from "./styles/PasswordChangeStyles";
+const Account = ({ user, Mockuser }) => {
+  const { deleteAccount, loading, error } = useAccount();
 
+//   console.log("User:", user);
 
-const Account = ({ user , Mockuser }) => {
-    return (
-        <div style={{marginTop : "2rem"}}>
-            <PersonalInfo user={user} Mockuser={Mockuser}/>
-            <PasswordChange user={user}/>
-            <div style={{ display: "flex", width: "95%", margin : "0 auto" , justifyContent : "end", marginBottom: "5rem" }}>
-                <Button type="button">회원 정보 변경</Button>
-                <DelBtn type="button">회원 탈퇴하기</DelBtn>
-              </div>
-        </div>
-    );
+  if (loading) {
+    return <p>로딩 중...</p>;
+  }
+
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <PersonalInfo user={user} Mockuser={Mockuser} />
+      <PasswordChange user={user} />
+      <div style={{ display: "flex", width: "95%", margin: "0 auto", justifyContent: "end", marginBottom: "5rem" }}>
+        <Button type="button">회원 정보 변경</Button>
+        <DelBtn type="button" onClick={deleteAccount}>회원 탈퇴하기</DelBtn>
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
 };
 
 export default Account;
