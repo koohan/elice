5ba8eb481d88af6ad8a1e801ab5a50e067afdc04
Lee gangho <rkdgh1190@gmail.com>
2024-06-14 23:52:38 +0900

    안쓰는 파일 삭제

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import PersonalInfo from './PersonalInfo';
 import PasswordChange from './PasswordChange';
 import { Button, DelBtn } from "./styles/PasswordChangeStyles";
-import useAccount from '../../hook/useAccount';
 
 const Account = ({ user, Mockuser }) => {
-  const { deleteAccount, loading, error } = useAccount();
 
-  if (loading) return <p>로딩 중...</p>;
 
   return (
     <div style={{ marginTop: "2rem" }}>
@@ -15,9 +12,8 @@ const Account = ({ user, Mockuser }) => {
       <PasswordChange user={user} />
       <div style={{ display: "flex", width: "95%", margin: "0 auto", justifyContent: "end", marginBottom: "5rem" }}>
         <Button type="button">회원 정보 변경</Button>
-        <DelBtn type="button" onClick={deleteAccount}>회원 탈퇴하기</DelBtn>
+        <DelBtn type="button" >회원 탈퇴하기</DelBtn>
       </div>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
   );
 };
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import ProductList from '../components/product/ProductList';
 import Sidebar from '../components/sidebar/Sidebar';
@@ -33,22 +32,12 @@ const ContentWrapper = styled.div`
   z-index: 1;
 `;
 
-const filterCategory = (data, categoryName) => {
-  if (!data ?? !categoryName) return data;
-  return data.filter(product => product.category?.name === categoryName);
-};
-
 const ProductPage = () => {
-  const { categoryName } = useParams();
-  const { data, loading, error } = useFetchData("/api/product");
+  const fetchUrl = '/api/product';
+  const { data, loading, error } = useFetchData(fetchUrl);
   console.log(data)
-  const [filteredData, setFilteredData] = useState([]);
 
-  useEffect(() => {
-    if (data) {
-      setFilteredData(filterCategory(data, categoryName));
-    }
-  }, [data, categoryName]);
+
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -63,9 +52,7 @@ const ProductPage = () => {
         <ContentLayout>
           <BackgroundWrapper>
             <BackgroundImage />
-            <ProductList products={data} filteredData={filteredData} />
-             {/* 오피스 아워 시간에 product안뜬 원인 data 필터링 해서 보내줘서 그럼 페이크 페이지를 구성해야 하는데 생각이 짧았어서 문제가 생김*/}
-             {/* 임시 방편으로 필터링 된 데이터만 전달해주는 동일한 컴포넌트 작성하거나 아예 배제해야할 듯 함*/}
+            <ProductList products={data} />
           </BackgroundWrapper>
         </ContentLayout>
       </PageLayout>
