95c71783644125214b0d237661df1887918c724e
[IOT3]조 현성 <dr.caesarclown@gmail.com>
2024-06-16 20:43:39 +0900

    링크

--- src/components/login/Oauth.jsx
+++ src/components/login/Oauth.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { OauthContainer, BtnContainer } from '../register/styles/OauthContainerStyles';
 
-const Oauth = () => (
+const Oauth = () => {
+  const handleGoogleLogin = () => {
+    window.location.href="/auth/google";
+  }
+  return(
   <OauthContainer>
-    <BtnContainer className='google-login'><img src="/assets/google.webp" alt="Google 로그인" /></BtnContainer>
+    <BtnContainer className='google-login' onClick={handleGoogleLogin}><img src="/assets/google.webp" alt="Google 로그인" /></BtnContainer>
     <BtnContainer className='naver-login'><img src="/assets/naver.webp" alt="Naver 로그인" /></BtnContainer>
     <BtnContainer className='kakao-login'><img src="/assets/kakao.webp" alt="Kakao 로그인" /></BtnContainer>
     <BtnContainer className='apple-login'><img src="/assets/apple.webp" alt="Apple 로그인" /></BtnContainer>
-  </OauthContainer>
-);
+  </OauthContainer>)
+};
 
 export default Oauth;
